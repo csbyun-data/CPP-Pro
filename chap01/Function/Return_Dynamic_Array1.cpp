91affa567835a8e6ff9749dcd233f1ded88fea76
csbyun-data <time_series@naver.com>
2024-11-29 22:08:46 +0900

    Update Return_Dynamic_Array1.cpp

--- chap01/Function/Return_Dynamic_Array1.cpp
+++ chap01/Function/Return_Dynamic_Array1.cpp
@@ -1,1 +1,26 @@
+// C++ Program to Demostrating returning of a local array
+// from a function Using dynamically allocated array
 
+#include <iostrem>
+using namespace std;
+
+int *fun() {
+  int *arr = new int[100];
+
+  // some operations on arr[]
+  arr[0]=10;
+  arr[1]=20;
+
+  return arr;
+}
+
+int main()
+{
+  int *ptr = fun();
+  cout << ptr[0] << " " << ptr[1];
+
+  // allocated memory must be deleted
+  delete[] ptr;
+
+  return 0;
+}
