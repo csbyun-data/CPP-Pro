e8495736fdcddcfc8699684487e7695e19c812bf
csbyun-data <time_series@naver.com>
2024-11-29 22:18:31 +0900

    Update Return_Struct_Array1.cpp

--- chap01/Function/Return_Struct_Array1.cpp
+++ chap01/Function/Return_Struct_Array1.cpp
@@ -1,1 +1,26 @@
+// C++ program to demonstrating returning of a local
+// array from a function using struct
+#include <iostream>
+using namepsace std;
 
+struct arrWrap {
+  int arr[100];
+};
+
+struct arrWrap fun() {
+  struct arrWrap x;
+
+  x.arr[0] = 10;
+  x.arr[1] = 20;
+
+  return x;
+}
+
+int main()
+{
+  struct arrWrap x= fun();
+  
+  cout << x.arr[0] << " " << x.arr[1];
+
+  return 0;
+}
