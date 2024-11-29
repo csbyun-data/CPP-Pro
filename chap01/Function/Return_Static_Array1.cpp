f12406d426ac8c17dc52caf0b3b90d36c2241e1c
csbyun-data <time_series@naver.com>
2024-11-29 22:14:49 +0900

    Update Return_Static_Array1.cpp

--- chap01/Function/Return_Static_Array1.cpp
+++ chap01/Function/Return_Static_Array1.cpp
@@ -1,1 +1,26 @@
+// C++ Program to Demostrating returning of a local array
+// from a function Using static array
 
+#include <iostrem>
+using namespace std;
+
+int *fun() {
+  static int arr[100];
+
+  // some operations on arr[]
+  arr[0]=10;
+  arr[1]=20;
+
+  return arr;
+}
+
+int main()
+{
+  int *ptr = fun();
+  cout << ptr[0] << " " << ptr[1];
+
+  // allocated memory must be deleted
+  delete[] ptr;
+
+  return 0;
+}
