c1ceaf25ecbe09617bcf773d6a5cb52b9fc0290f
csbyun-data <time_series@naver.com>
2024-12-01 21:37:43 +0900

    Create Return_Dynamic_2D_Array1.cpp

---
+++ chap01/Function/Return_Dynamic_2D_Array1.cpp
@@ -0,0 +1,50 @@
+// C++ code for returning 2D array from function using Dynamic array
+#include <iostream>
+
+using namespace std;
+
+const int N=3;
+
+// function to display array
+void printArray(int **arr) {
+  for( int i=0; i<N; ++i) {
+    for( int j=0; j<N; ++j) {
+      cout << arr[i][j]<<" ";
+    }
+    cout << endl;
+  }
+}
+
+// function to initialize and returning array
+int **getArray() {
+  int **arr = new int*[N];
+
+  for( int i=0; i<N; ++i) {
+    arr[i] = new int[N];
+    for( int j=0; j<N; ++j) {
+      arr[i][j] = i+j;
+    }
+  }
+  return arr;
+}
+
+int main()
+{
+  int **arr;
+  arr = getArray();
+
+  printArray( arr);
+
+  // Delete the array created
+  for( int i-0; i<N; i++) // To delete the inner arrays
+    delete[] arr[i];
+  delete[] arr; // To delete the outer array which contained the pointers
+                // of all the inner arrays
+  return 0;
+}
+
+// 0 1 2
+// 1 2 3
+// 2 3 4
+
+  
