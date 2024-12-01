e0cccdb0028c3b8ff1619f07a90220031ae0b478
csbyun-data <time_series@naver.com>
2024-12-01 20:58:43 +0900

    Update Return_Struct_Array1.cpp

--- chap01/Function/Return_Struct_Array1.cpp
+++ chap01/Function/Return_Struct_Array1.cpp
@@ -1,6 +1,8 @@
 // C++ program to demonstrating returning of a local
 // array from a function using struct
 // https://www.geeksforgeeks.org/how-to-return-a-local-array-from-a-cpp-function/
+// struct의 멤버 배열이 깊이 복사가 되어 return value로 전달됨
+// 지역변수가 메모리에서 제거되어도 전달됨
 
 #include <iostream>
 using namepsace std;
