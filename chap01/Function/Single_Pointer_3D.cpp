78eae15a896288f2fa947ace79af08d954b9da00
csbyun-data <time_series@naver.com>
2024-12-01 22:20:01 +0900

    Create Single_Pointer_3D.cpp

---
+++ chap01/Function/Single_Pointer_3D.cpp
@@ -0,0 +1,45 @@
+// C++ program to dynamically allocate
+// the memory for 3D array in C++ using new operator
+// https://www.geeksforgeeks.org/how-to-dynamically-allocate-a-3d-array-in-c/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Dimensions of the 3D array
+  int x=2, y=3, z=4;
+  int count=0;
+
+  // Allocate memory blocks of size x*y*z
+  int *a = new int[ x*y*z];
+
+  // Traverse the 3D array
+  for( int i=0; i<x; i++) {
+    for( int j=0; j<y; j++) {
+      for( int k=0; k<z; k++) {
+        // Assign values to the memory blocks created
+        *(a + i*y*z + j*z + k) = ++count;
+      }
+    }
+  }
+
+ // Traverse the 3D array
+  for( int i=0; i<x; i++) {
+    for( int j=0; j<y; j++) {
+      for( int k=0; k<z; k++) {
+        // Print values of the memory blocks created
+        cout << *(a + i*y*z + j*z + k) << " ";
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+
+  // Deallocate memory
+  delete[] a;
+
+  return 0;
+}
+
+
