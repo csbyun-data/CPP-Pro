c9715cf66eefc401a7fef0492477446d7588b772
csbyun-data <time_series@naver.com>
2024-12-02 22:40:42 +0900

    Create Functor1.cpp

---
+++ chap02/Function_Pointer/Functor1.cpp
@@ -0,0 +1,21 @@
+// Functor의 사용, 함수를 객체로 사용
+// 연산자의 overloading
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Square_Functor {
+  void operator() ( int x) // ()연산자 overloading
+  { std::cout << x*x << " "; }
+};
+
+int main()
+{
+  Square_Functor square;
+  vector<int> vec { 1, 2, 3, 4 };
+
+  for_each( vec.begin(), vec.end(), square);
+}
