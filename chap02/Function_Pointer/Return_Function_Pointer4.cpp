042a78ccf83d40c3450ffc7f0f7059fa17926edd
csbyun-data <time_series@naver.com>
2024-12-02 23:21:36 +0900

    Create Return_Function_Pointer4.cpp

---
+++ chap02/Function_Pointer/Return_Function_Pointer4.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int add(int a, int b) {
+  return a + b;
+}
+ 
+int subtract(int a, int b) {
+  return a - b;
+}
+ 
+int (*operation(char op))(int, int) {
+  if (op == '+') {
+    return &add;
+  } else if (op == '-') {
+    return &subtract;
+  } else {
+    return NULL;
+  }
+}
+ 
+int main() {
+  int (*func_ptr)(int, int) = operation('+');
+  printf("%d\n", func_ptr(4, 5));
+
+  return 0;
+}
+
+// 9
