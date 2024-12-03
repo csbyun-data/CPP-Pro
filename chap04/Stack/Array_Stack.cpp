b5218375fc3a59ef9904934f33f7ca8a011ee804
csbyun-data <time_series@naver.com>
2024-12-03 12:58:25 +0900

    Update Array_Stack.cpp

--- chap04/Stack/Array_Stack.cpp
+++ chap04/Stack/Array_Stack.cpp
@@ -1,51 +1,52 @@
 #include"Array_Stack.h"
 
 // 매개 변수 (ArrayStack 구조체 , 스택의 용량)
-void AS_CreateStack(ArrayStack** Stack, int Capacity)
-{
-	// 스택을 자유 저장소에 생성
-	(*Stack) = (ArrayStack*)malloc(sizeof(ArrayStack));
+void AS_CreateStack(ArrayStack** Stack, int Capacity) {
+  // 스택을 자유 저장소에 생성
+  (*Stack) = (ArrayStack*)malloc(sizeof(ArrayStack));
 
-	// 입력된 Capacity 만큼의 노드를 자유 저장소에 생성
-	(*Stack)->Nodes = (Node*)malloc(sizeof(Node) * Capacity);
+  // 입력된 Capacity 만큼의 노드를 자유 저장소에 생성
+  (*Stack)->Nodes = (Node*)malloc(sizeof(Node) * Capacity);
 
-	// Capacity 및 Top 초기화
-	(*Stack)->Capacity = Capacity;
-	(*Stack)->Top = -1;
+  // Capacity 및 Top 초기화
+  (*Stack)->Capacity = Capacity;
+  (*Stack)->Top = -1;
 }
 
-void AS_DestroyStack(ArrayStack* Stack)
-{
-	// 노드를 자유 저장소에서 해제
-	free(Stack->Nodes);
+void AS_DestroyStack(ArrayStack* Stack) {
+  // 노드를 자유 저장소에서 해제
+  free(Stack->Nodes);
 
-	// 스택을 자유 저장소에서 해제
-	free(Stack);
+  // 스택을 자유 저장소에서 해제
+  free(Stack);
 }
 
-void AS_Push(ArrayStack* Stack, ElementType Data)
-{
-	Stack->Top++;
-	Stack->Nodes[Stack->Top].Data = Data;
+void AS_Push(ArrayStack* Stack, ElementType Data) {
+  Stack->Top++;
+  Stack->Nodes[Stack->Top].Data = Data;
 }
 
-ElementType AS_Pop(ArrayStack* Stack)
-{
-	int Position = Stack->Top--;
-	return Stack->Nodes[Position].Data;
+ElementType AS_Pop(ArrayStack* Stack) {
+  int Position = Stack->Top--;
+  return Stack->Nodes[Position].Data;
 }
 
-ElementType AS_Top(ArrayStack* Stack)
-{
-	return Stack->Nodes[Stack->Top].Data;
+ElementType AS_Top(ArrayStack* Stack) {
+  return Stack->Nodes[Stack->Top].Data;
 }
 
-int AS_GetSize(ArrayStack* Stack)
-{
-	return Stack->Top + 1;
+int AS_GetSize(ArrayStack* Stack) {
+  return Stack->Top + 1;
 }
 
-int AS_IsEmpty(ArrayStack* Stack)
-{
-	return (Stack->Top == -1);
+int AS_IsEmpty(ArrayStack* Stack) {
+  return (Stack->Top == -1);
 }
+
+// 함수 정의 ArrayStack.cpp 에 추가
+int AS_IsFull( ArrayStack* Stack) {
+  return (Stack->Top == Stack->Capacity);
+}
+
+
+
