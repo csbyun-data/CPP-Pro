0ac1d566b66e536c2f008e4918b629ec76d42775
csbyun-data <time_series@naver.com>
2024-12-05 15:05:43 +0900

    Create CBase1.cpp

---
+++ chap01/Library/CBase1.cpp
@@ -0,0 +1,22 @@
+// 1. Virtual Function code
+
+#include <stdio.h>
+
+class CBase {
+  public:
+    virtual void WhoAreYou() {
+      printf("CBase\n");
+    }
+};
+
+CBase theApp;
+CBase* AfxGetApp() {
+  return &theApp;
+}
+
+void main()
+{
+  CBase* p;
+  p = AfxGetApp();
+  p->WhoAreYou();
+}
