dcac49f135a11b1b181cd0cacc62611178fab92e
csbyun-data <time_series@naver.com>
2024-12-06 14:34:04 +0900

    Update Array_Pointer3.cpp

--- chap01/Const/Array_Pointer3.cpp
+++ chap01/Const/Array_Pointer3.cpp
@@ -4,13 +4,11 @@ using namespace std;
 
 int main()
 {
-	int arr[] = { 10, 20, 30};
-	
-	cout << sizeof(arr) << endl; // sizeof(int) * 3
-	
-	int* arr_ptr = arr;          // arr_ptr의 주소의 저장되 변수 크기 
-	
-	cout << sizeof(arr_ptr) << endl;
+  int arr[] = { 10, 20, 30};
+  cout << sizeof(arr) << endl; // sizeof(int) * 3
+
+  int* arr_ptr = arr;          // arr_ptr의 주소의 저장되 변수 크기 
+  cout << sizeof(arr_ptr) << endl;
 }
 
 /*
