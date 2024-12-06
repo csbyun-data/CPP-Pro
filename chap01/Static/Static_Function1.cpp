8d8fe9c9cd61f0542c0872e56f44ea2ee5d6cc71
csbyun-data <time_series@naver.com>
2024-12-06 13:46:33 +0900

    Create Static_Function1.cpp

---
+++ chap01/Static/Static_Function1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class My {
+public:
+	static void staticFunc() {
+		cout << "Static function. " << endl;
+	}
+	
+	void nonStaticFunc() {
+		cout << "nonStatic function. " << endl;	
+	}
+};
+
+int main() {
+	
+	// calling static function
+	My::staticFunc();
+	
+	// calling non-static function
+	My obj;
+	obj.nonStaticFunc();
+	
+	return 0;	
+}
+
+// Static function.
+// nonStatic function.
