634a89eeb4c2ae7d7b71c7e51af32ceecd387b6f
csbyun-data <time_series@naver.com>
2024-12-06 16:39:54 +0900

    Create method_chain1.cpp

---
+++ chap01/Method_Chaining/method_chain1.cpp
@@ -0,0 +1,35 @@
+// https://j2doll.tistory.com/773
+// https://github.com/j2doll/method-chaining-cpp
+
+#include <iostream>
+#include <string>
+
+class User {
+public:
+    User& setName(const std::string& name) {
+        this->name = name;
+        return *this;
+    }
+
+    User& setAge(int age) {
+        this->age = age;
+        return *this;
+    }
+
+    void print() const {
+        std::cout << "Name: " << name << ", Age: " << age << std::endl;
+    }
+
+protected:
+    std::string name;
+    int age;
+};
+
+int main() {
+    User user;
+    user.setName("Alice")
+        .setAge(25)
+        .print();
+
+    return 0;
+}
