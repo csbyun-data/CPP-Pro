d0a7a283d6ddbe753f8facaaff446db5e1da1b8e
csbyun-data <time_series@naver.com>
2024-12-06 09:42:03 +0900

    Update Call_by_reference1.cpp

--- chap01/Function/Call_by_reference1.cpp
+++ chap01/Function/Call_by_reference1.cpp
@@ -15,15 +15,3 @@ void main()
   int iy = 3;
   swap(ix, iy);
 }
-
-// Reference Variable 처리
-  int a;
-  int& b = a;
-
-// function 처리
-void Ref(int& b) {}  //2. 처리
-void main()
-{
-  int a = 2;
-  Ref(a);  //1. int& b = a;
-}
