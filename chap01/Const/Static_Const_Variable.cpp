f9acb138c83119afc2717bdafbc7d0ddd46c8f18
csbyun-data <time_series@naver.com>
2024-12-06 14:21:15 +0900

    Create Static_Const_Variable.cpp

---
+++ chap01/Const/Static_Const_Variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class X {
+	public:
+		static const int a;
+};
+
+const int X::a=3;  // 반드시 변수 재선언과 초기값 입력
+
+int main()
+{
+	cout << X::a << endl;
+	
+	return 0;
+}
