679949a9418afab954984b423e35b1e330935fac
csbyun-data <time_series@naver.com>
2024-12-06 13:50:21 +0900

    Update Static_Function1.cpp

--- chap01/Static/Static_Function1.cpp
+++ chap01/Static/Static_Function1.cpp
@@ -3,26 +3,25 @@
 using namespace std;
 
 class My {
-public:
-	static void staticFunc() {
-		cout << "Static function. " << endl;
-	}
-	
-	void nonStaticFunc() {
-		cout << "nonStatic function. " << endl;	
-	}
+  public:
+    static void staticFunc() {
+      cout << "Static function. " << endl;
+    }
+
+    void nonStaticFunc() {
+      cout << "nonStatic function. " << endl;	
+    }
 };
 
 int main() {
-	
-	// calling static function
-	My::staticFunc();
-	
-	// calling non-static function
-	My obj;
-	obj.nonStaticFunc();
-	
-	return 0;	
+  // calling static function
+  My::staticFunc();
+
+  // calling non-static function
+  My obj;
+  obj.nonStaticFunc();
+
+  return 0;	
 }
 
 // Static function.
