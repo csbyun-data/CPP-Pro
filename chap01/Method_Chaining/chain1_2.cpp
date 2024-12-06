5a2e771018713b333fd5d96eb0710b18283bf2a2
csbyun-data <time_series@naver.com>
2024-12-06 16:53:40 +0900

    Create chain1_2.cpp

---
+++ chap01/Method_Chaining/chain1_2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+class Calculation {
+	int a;
+public:
+	Calculation(const int& a_in)
+		: a(a_in)
+	{}
+	Calculation& add(const int& b) { a += b; return *this; }
+	Calculation& sub(const int& b) { a -= b; return *this; }
+	Calculation& mul(const int& b) { a *= b; return *this; }
+	int getnum() { return a; }
+	void print() { cout << a << endl; }
+};
+
+int main() {
+	Calculation calc(0);
+	calc.add(5).sub(2).mul(6).print();
+}
+
+
