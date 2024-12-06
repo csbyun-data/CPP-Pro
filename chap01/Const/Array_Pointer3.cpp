6836a8eaa84662525652c1e7a183715904b1b19e
csbyun-data <time_series@naver.com>
2024-12-06 14:24:07 +0900

    Create Array_Pointer3.cpp

---
+++ chap01/Const/Array_Pointer3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = { 10, 20, 30};
+	
+	cout << sizeof(arr) << endl; // sizeof(int) * 3
+	
+	int* arr_ptr = arr;          // arr_ptr의 주소의 저장되 변수 크기 
+	
+	cout << sizeof(arr_ptr) << endl;
+}
+
+/*
+12
+8
+*/
