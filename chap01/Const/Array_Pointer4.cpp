a75381c23aa21324f2bf59884df1bfcbacc72271
csbyun-data <time_series@naver.com>
2024-12-06 14:26:15 +0900

    Create Array_Pointer4.cpp

---
+++ chap01/Const/Array_Pointer4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[] = { 10, 20, 30};
+	int* arr_ptr = arr;
+	
+	cout << arr_ptr << endl;
+	cout << (arr_ptr + 1) << endl;
+	cout << (arr_ptr + 2) << endl;
+	
+	cout << *arr_ptr << endl;
+	cout << *(arr_ptr + 1) << endl;
+	cout << *(arr_ptr + 2) << endl;
+}
+
+/*
+0x6ffe00
+0x6ffe04
+0x6ffe08
+10
+20
+30
+*/
