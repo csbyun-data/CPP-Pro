459b87594f9f3040a999355f898c1a6f73f06e42
csbyun-data <time_series@naver.com>
2024-12-12 12:17:13 +0900

    Update File_IO1.cpp

--- chap03/File_IO1.cpp
+++ chap03/File_IO1.cpp
@@ -8,30 +8,31 @@
 using namespace std;
 
 int main() {
-	char str[100];
-	
-	// Creates an instance of ofstream, and opens a.txt
-	ofstream a_file("a.txt");
-	// Outputs to a.txt through a_file
-	a_file << "This text will now be inside of a.txt";
-	a_file.close();
-	
-	// Opens for reading the file
-	ifstream b_file("a.txt");
-	
-	if( !b_file.is_open()) {
-		// The file could not be opened
-	} else {
-		// Safely use the file stream
-		
-		// Reads one string from the file
-		b_file >> str;
-		// should output 'this'
-		cout << str << '\n';
-	}
-	cin.get();    // wait for a keypress
-	// b_file is closed implicitly here
-	// b_file.close();
+
+  char str[100];
+
+  // Creates an instance of ofstream, and opens a.txt
+  ofstream a_file("a.txt");
+
+  // Outputs to a.txt through a_file
+  a_file << "This text will now be inside of a.txt";
+  a_file.close();
+
+  // Opens for reading the file
+  ifstream b_file("a.txt");
+
+  if( !b_file.is_open()) {
+    // The file could not be opened
+  } else {
+    // Safely use the file stream
+
+    // Reads one string from the file should output 'this'
+    b_file >> str;
+    cout << str << '\n';
+  }
+  cin.get();    // wait for a keypress
+  // b_file is closed implicitly here
+  b_file.close();
 	
 	return 0;
 }
