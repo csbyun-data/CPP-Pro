caa68300a4e99890457741f3e36dc69771269ca4
csbyun-data <time_series@naver.com>
2024-12-12 12:52:25 +0900

    Create Binary_File3.cpp

---
+++ chap03/File/Binary_File3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	ofstream fop("test.bin", ios_base::out|ios_base::binary);
+	
+	char a = 'A';
+	char b = 'B';
+	char c = 'C';
+	
+	char str[6] = "abcde";
+	
+	fop.write(&a, sizeof(char));
+	fop.write(str, sizeof(str)-1);
+	fop.write(&b, sizeof(b));
+	
+	fop << "Hello" << ' ';
+	fop.put('7');
+	
+	fop.write(&c, 1);
+	fop.close();
+	
+	return 0;
+}
