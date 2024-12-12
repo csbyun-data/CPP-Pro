74e173d227e0cd4baf6343dec850c9d713734b77
csbyun-data <time_series@naver.com>
2024-12-12 14:17:44 +0900

    Update Binary_File4.cpp

--- chap03/File/Binary_File4.cpp
+++ chap03/File/Binary_File4.cpp
@@ -5,22 +5,22 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	ofstream fop("test.bin", ios_base::out|ios_base::binary);
-	
-	int a = 143;
-	int b = 27;
-	double c = 3.14;
-	
-	int arr[3] = {2, 4, 6};
-	string str = "Hello";
-	
-	fop.write((char *)&a, sizeof(int));
-	fop.write((char *)arr, sizeof(arr));
-	
-	fop << b << c << str;
-	
-	fop.close();
-	
-	return 0;
+int main(int argc, char** argv) 
+{
+  ofstream fop("test.bin", ios_base::out|ios_base::binary);
+
+  int a = 143;
+  int b = 27;
+  double c = 3.14;
+
+  int arr[3] = {2, 4, 6};
+  string str = "Hello";
+
+  fop.write((char *)&a, sizeof(int));
+  fop.write((char *)arr, sizeof(arr));
+
+  fop << b << c << str;
+  fop.close();
+
+  return 0;
 }
