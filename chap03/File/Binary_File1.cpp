9958bcae9aaec96845b3fca095997515ffe808d6
csbyun-data <time_series@naver.com>
2024-12-12 12:19:20 +0900

    Create Binary_File1.cpp

---
+++ chap03/File/Binary_File1.cpp
@@ -0,0 +1,23 @@
+//파일출력 스트림 객체 생성
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(void)
+{
+	char fname[] = "a.dat";
+	int age = 21;
+	char name[] = "name1";
+
+	ofstream fout(fname); 
+	if(!fout) {
+		cout << "Failed to open file"  << endl;
+		return 0;
+	}
+	
+	fout << age << '\n';
+	fout << name << endl;
+
+    fout.close()
+}
