79b19ab7668a2f2008efa6c96852c1f22b44e2dc
csbyun-data <time_series@naver.com>
2024-12-12 14:15:18 +0900

    Update Binary_File4_in.cpp

--- chap03/File/Binary_File4_in.cpp
+++ chap03/File/Binary_File4_in.cpp
@@ -6,29 +6,28 @@ using namespace std;
 
 int main() 
 {
-	ifstream fip("test.bin", ios_base::in|ios_base::binary);
-	
-	int a, b;
-	double c;
-	int arr[3];
-	string str;
-	
-	fip.read((char *)&a, sizeof(int));
-	fip.read((char *)arr, sizeof(arr));
-	fip >> b >> c >> str;
-	
-	cout << "int a: " << a << endl;
-	cout << "int b: " << b << endl;
-	cout << "double c: " << c << endl;
-	
-	cout << "int arr[3]: ";
-	for( int i=0; i<3; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
-	
-	cout << "string str: " << str << endl;
-	
-	fip.close();
-	
-	return 0;
+  ifstream fip("test.bin", ios_base::in|ios_base::binary);
+
+  int a, b;
+  double c;
+  int arr[3];
+  string str;
+
+  fip.read((char *)&a, sizeof(int));
+  fip.read((char *)arr, sizeof(arr));
+  fip >> b >> c >> str;
+
+  cout << "int a: " << a << endl;
+  cout << "int b: " << b << endl;
+  cout << "double c: " << c << endl;
+
+  cout << "int arr[3]: ";
+  for( int i=0; i<3; i++)
+    cout << arr[i] << ' ';
+  cout << endl;
+
+  cout << "string str: " << str << endl;
+  fip.close();
+
+  return 0;
 }
