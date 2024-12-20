1ced90f0087349d1fb2141bd72b38495f480e917
csbyun-data <time_series@naver.com>
2024-12-21 00:19:57 +0900

    Create sort2_04.cpp

---
+++ chap05/sort/sort2_04.cpp
@@ -0,0 +1,30 @@
+/* The following code example is taken from the book
+ * "The C++ Standard Library - A Tutorial and Reference"
+ * by Nicolai M. Josuttis, Addison-Wesley, 1999
+ *
+ * (C) Copyright Nicolai M. Josuttis 1999.
+ * Permission to copy, use, modify, sell and distribute this software
+ * is granted provided this copyright notice appears in all copies.
+ * This software is provided "as is" without express or implied
+ * warranty, and with no claim as to its suitability for any purpose.
+ */
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    int coll[] = { 5, 6, 2, 4, 1, 3 };
+
+    // sort beginning with the second element
+    sort (coll+3, coll+6);
+
+    // print all elements
+    copy (coll, coll+6,ostream_iterator<int>(cout," "));
+    cout << endl;
+}
+
+// run result
+5 6 2 1 3 4
