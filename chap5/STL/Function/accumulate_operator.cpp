820c33dbbf447db592d02e2a7945c0ab8ff77b27
csbyun-data <time_series@naver.com>
2024-12-20 12:29:18 +0900

    Update accumulate_operator.cpp

--- chap5/STL/Function/accumulate_operator.cpp
+++ chap5/STL/Function/accumulate_operator.cpp
@@ -1,3 +1,5 @@
+// accumulator 함수는 배열의 합, 리스트의 모든 원소의 합, 곱.. 등 나열된 원소의 특정한 연산을 할때 사용
+
 #include <iostream>
 
 #include <numeric>
@@ -5,10 +7,9 @@
 
 using std::accumulate;
 
-class CAdd
-{
-	public:
-		int operator()(int x, int y) const { return x + y; }
+class CAdd {
+  public:
+    int operator()(int x, int y) const { return x + y; }
 };
 
 // CAdd Class 활용 예
@@ -17,13 +18,10 @@ class CAdd
 
 
 int main(int argc, char** argv) {
-	int i[10] = {1, 2, 3,  4, 5, 6, 7, 8, 9, 10};
-	
-	int product = accumulate(&i[0], &i[10], 0, CAdd());
-	
-	assert( 55 == product);
-	
-	return 0;
+  int i[10] = {1, 2, 3,  4, 5, 6, 7, 8, 9, 10};
+
+  int product = accumulate(&i[0], &i[10], 0, CAdd());
+  assert( 55 == product);
+
+  return 0;
 }
-// accumulate의 함수 예
-inline int accumulate (int* _F, int* _L, int _V, CAdd _B);
