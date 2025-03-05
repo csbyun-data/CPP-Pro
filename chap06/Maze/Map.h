965568025c076cfaaaf8a8fbbee451cad2b0c766
csbyun-data <time_series@naver.com>
2025-03-05 23:56:16 +0900

    Update Map.h

--- chap06/Maze/Map.h
+++ chap06/Maze/Map.h
@@ -10,30 +10,30 @@
 using namespace std;
 
 class Map {
-	private:
-		int count;
-		int width;
-		int height;
-		int targetX;
-		int targetY;
-		char** map2dArray; //initialize in build()
-		
-		bool makeMap2dArray();
-		void openWay(int startXPositin , int startYPosition);
-	public:
-		Map();
-		~Map();
-		void setMapDimens(const int WIDTH , const int HEIGHT);
-		int getMapWidth();
-		int getMapHeight();
-		int setMapHeight();
-		void build();
-		void draw();
-		char** getMap2dArray();
-		static const char WALL = '|' ; //char(219);
-		static const char BOMB = '0';
-		static const char FREE = ' ';
-		static const char TARGET = 'O';
+  private:
+    int count;
+    int width;
+    int height;
+    int targetX;
+    int targetY;
+    char** map2dArray; //initialize in build()
+    
+    bool makeMap2dArray();
+    void openWay(int startXPositin , int startYPosition);
+  public:
+    Map();
+    ~Map();
+    void setMapDimens(const int WIDTH , const int HEIGHT);
+    int getMapWidth();
+    int getMapHeight();
+    int setMapHeight();
+    void build();
+    void draw();
+    char** getMap2dArray();
+    static const char WALL = '|' ; //char(219);
+    static const char BOMB = '0';
+    static const char FREE = ' ';
+    static const char TARGET = 'O';
 };
 
 #endif
