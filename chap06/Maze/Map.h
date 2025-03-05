249e0dc05613707877826091070b770eef796422
csbyun-data <time_series@naver.com>
2025-03-05 23:48:33 +0900

    Create Map.h

---
+++ chap06/Maze/Map.h
@@ -0,0 +1,39 @@
+#ifndef MAP_H_INCLUDED
+#define MAP_H_INCLUDED
+
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+#include <cstdlib>
+#include "ConsoleController.h"
+
+using namespace std;
+
+class Map {
+	private:
+		int count;
+		int width;
+		int height;
+		int targetX;
+		int targetY;
+		char** map2dArray; //initialize in build()
+		
+		bool makeMap2dArray();
+		void openWay(int startXPositin , int startYPosition);
+	public:
+		Map();
+		~Map();
+		void setMapDimens(const int WIDTH , const int HEIGHT);
+		int getMapWidth();
+		int getMapHeight();
+		int setMapHeight();
+		void build();
+		void draw();
+		char** getMap2dArray();
+		static const char WALL = '|' ; //char(219);
+		static const char BOMB = '0';
+		static const char FREE = ' ';
+		static const char TARGET = 'O';
+};
+
+#endif
