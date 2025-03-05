4a7f6be36f31a0fc2b6102fb64fa26b3fa55e2e1
csbyun-data <time_series@naver.com>
2025-03-06 00:01:59 +0900

    Update Avatar.cpp

--- chap06/Maze/Avatar.cpp
+++ chap06/Maze/Avatar.cpp
@@ -1,144 +1,155 @@
 #include "Avatar.h"
 #include "Map.h"
 
-Avatar::Avatar(int xBorder ,int yBorder){
-	x = 1;
-	y = 1;
-	this->xBorder = xBorder;
-	this->yBorder = yBorder;
-	map2dArray = NULL;
+Avatar::Avatar(int xBorder ,int yBorder) {
+  x = 1;
+  y = 1;
+  this->xBorder = xBorder;
+  this->yBorder = yBorder;
+  map2dArray = NULL;
 }
-Avatar::~Avatar(){
+
+Avatar::~Avatar() {
 }
-void Avatar::move(const int KEY){
-	if(isKeyAllowed(KEY)){
-		switch(KEY){
-			case UP_W:
-			case UP_ARROW: moveUp();
-				break;
-			case DOWN_S:
-			case DOWN_ARROW: moveDown();
-				break;
-			case LEFT_A:
-			case LEFT_ARROW: moveLeft();
-				break;
-			case RIGHT_D:
-			case RIGHT_ARROW: moveRight();
-				break;
-		}
-		winCheck();
-		bombCheck();
-	}
+
+void Avatar::move(const int KEY) {
+  if(isKeyAllowed(KEY)){
+    switch(KEY){
+      case UP_W:
+      case UP_ARROW: moveUp();
+        break;
+      case DOWN_S:
+      case DOWN_ARROW: moveDown();
+        break;
+      case LEFT_A:
+      case LEFT_ARROW: moveLeft();
+        break;
+      case RIGHT_D:
+      case RIGHT_ARROW: moveRight();
+        break;
+    }
+    winCheck();
+    bombCheck();
+  }
 }
-void Avatar::winCheck(){
-	if((map2dArray[y])[x] == Map::BOMB){
-		ConsoleController::clearScreen();
-		cout << "You lose!";
-		exit(1);
-	}
+
+void Avatar::winCheck() {
+  if((map2dArray[y])[x] == Map::BOMB) {
+    ConsoleController::clearScreen();
+    cout << "You lose!";
+    exit(1);
+  }
 }
-void Avatar::bombCheck(){
-	if(x == xBorder-2 && y == yBorder-2){
-		ConsoleController::clearScreen();
-		printYouWin();
-		exit(1);
-	}
+void Avatar::bombCheck() {
+  if(x == xBorder-2 && y == yBorder-2) {
+    ConsoleController::clearScreen();
+    printYouWin();
+    exit(1);
+  }
 }
-void Avatar::printYouWin(){
-	cout << " ##      ##         ###            ##        ## \n"
-		 << "  ##    ##      ###     ###        ##        ## \n"
-		 << "   ##  ##     ##           ##      ##        ## \n"
-		 << "    ####     ##             ##     ##        ## \n"
-		 << "     ##     ##               ##    ##        ## \n"
-		 << "     ##      ##             ##     ##        ## \n"
-		 << "     ##       ##           ##      ##        ## \n"
-		 << "     ##         ###     ###         ##      ##  \n"
-		 << "     ##             ###                ####   \n\n";
-			
-	cout << "###                        ###  ####     ####      ### \n"
-		 << " ##           ##           ##    ##	  ###       ## \n"
-		 << "  ##         ####         ##     ##	  ## ##     ## \n"
-		 << "   ##       ##  ##       ##      ##	  ##  ##    ## \n"
-		 << "    ##     ##    ##     ##       ##	  ##   ##   ## \n"
-		 << "     ##   ##      ##   ##        ##	  ##    ##  ## \n"
-		 << "      ## ##        ## ##         ##	  ##     ## ## \n"
-		 << "       ###          ###          ##	  ##       ### \n"
-		 << "        #            #          ####     ###      #### \n";
+void Avatar::printYouWin() {
+  cout << " ##      ##         ###            ##        ## \n"
+     << "  ##    ##      ###     ###        ##        ## \n"
+     << "   ##  ##     ##           ##      ##        ## \n"
+     << "    ####     ##             ##     ##        ## \n"
+     << "     ##     ##               ##    ##        ## \n"
+     << "     ##      ##             ##     ##        ## \n"
+     << "     ##       ##           ##      ##        ## \n"
+     << "     ##         ###     ###         ##      ##  \n"
+     << "     ##             ###                ####   \n\n";
+      
+  cout << "###                        ###  ####     ####      ### \n"
+     << " ##           ##           ##    ##	  ###       ## \n"
+     << "  ##         ####         ##     ##	  ## ##     ## \n"
+     << "   ##       ##  ##       ##      ##	  ##  ##    ## \n"
+     << "    ##     ##    ##     ##       ##	  ##   ##   ## \n"
+     << "     ##   ##      ##   ##        ##	  ##    ##  ## \n"
+     << "      ## ##        ## ##         ##	  ##     ## ## \n"
+     << "       ###          ###          ##	  ##       ### \n"
+     << "        #            #          ####     ###      #### \n";
 }
-void Avatar::moveUp(){
-	if(y != 0)
-		if(!isWallAt(Direction::TOP))
-			--y;
+
+void Avatar::moveUp() {
+  if(y != 0)
+    if(!isWallAt(Direction::TOP))
+      --y;
 }
-void Avatar::moveDown(){
-	bool condit = yBorder-1 != y;
-	if(condit)
-		if(!isWallAt(Direction::BOTTOM))
-			++y;
+
+void Avatar::moveDown() {
+  bool condit = yBorder-1 != y;
+  if(condit)
+    if(!isWallAt(Direction::BOTTOM))
+      ++y;
 }
-void Avatar::moveLeft(){
-	if(x != 0)
-		if(!isWallAt(Direction::LEFT))
-			--x;
+
+void Avatar::moveLeft() {
+  if(x != 0)
+    if(!isWallAt(Direction::LEFT))
+      --x;
 }
-void Avatar::moveRight(){
-	if(xBorder-1 != x)
-		if(!isWallAt(Direction::RIGHT))
-			++x;
+
+void Avatar::moveRight() {
+  if(xBorder-1 != x)
+    if(!isWallAt(Direction::RIGHT))
+      ++x;
 }
-bool Avatar::isKeyAllowed(const int KEY){
-	switch(KEY){
-		case UP_W:
-		case UP_ARROW:
-		case DOWN_S:
-		case DOWN_ARROW:
-		case LEFT_A:
-		case LEFT_ARROW:
-		case RIGHT_D:
-		case RIGHT_ARROW:
-			return true;
-			break;
-		default: return false;
-			break;
-	}
+
+bool Avatar::isKeyAllowed(const int KEY) {
+  switch(KEY){
+    case UP_W:
+    case UP_ARROW:
+    case DOWN_S:
+    case DOWN_ARROW:
+    case LEFT_A:
+    case LEFT_ARROW:
+    case RIGHT_D:
+    case RIGHT_ARROW:
+      return true;
+      break;
+    default: return false;
+      break;
+  }
 }
-void Avatar::draw(){
-	ConsoleController::goTo(x, y);
-	cout << Avatar::ANDICATOR;
+
+void Avatar::draw() {
+  ConsoleController::goTo(x, y);
+  cout << Avatar::ANDICATOR;
 }
-void Avatar::setMap(char** map2dArray){
-	this->map2dArray = map2dArray;
+
+void Avatar::setMap(char** map2dArray) {
+  this->map2dArray = map2dArray;
 }
-bool Avatar::isWallAt(Direction direction){
-	if(map2dArray != NULL){
-		switch (direction){
-			case Direction::TOP :{
-				if ((map2dArray[y-1])[x] == Map::WALL)
-					return true;
-				else
-					return false;
-			}
-			case Direction::BOTTOM :{
-				if ((map2dArray[y+1])[x] == Map::WALL)
-					return true;
-				else
-					return false;
-			}
-			case Direction::LEFT :{
-				if ((map2dArray[y])[x-1] == Map::WALL)
-					return true;
-				else
-					return false;
-			}
-			case Direction::RIGHT :{
-				if ((map2dArray[y])[x+1] == Map::WALL)
-					return true;
-				else
-					return false;
-			}
-			default: return false;
-		}
-	}else{
-		throw logic_error("didn't set map for avatar, use setMap()");
-	}
+
+bool Avatar::isWallAt(Direction direction) {
+  if(map2dArray != NULL) {
+    switch (direction) {
+      case Direction::TOP : {
+        if ((map2dArray[y-1])[x] == Map::WALL)
+          return true;
+        else
+          return false;
+      }
+      case Direction::BOTTOM : {
+        if ((map2dArray[y+1])[x] == Map::WALL)
+          return true;
+        else
+          return false;
+      }
+      case Direction::LEFT : {
+        if ((map2dArray[y])[x-1] == Map::WALL)
+          return true;
+        else
+          return false;
+      }
+      case Direction::RIGHT : {
+        if ((map2dArray[y])[x+1] == Map::WALL)
+          return true;
+        else
+          return false;
+      }
+      default: return false;
+    }
+  } else {
+    throw logic_error("didn't set map for avatar, use setMap()");
+  }
 }
