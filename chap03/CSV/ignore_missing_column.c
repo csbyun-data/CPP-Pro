ee4bd728b7cd8cbd07248cdf553b62c863db1faf
csbyun-data <time_series@naver.com>
2025-03-12 16:39:43 +0900

    Create ignore_missing_column.c

---
+++ chap03/CSV/ignore_missing_column.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "csv.h"
+
+int main()
+{
+	// The file only contains column "a"
+	io::CSVReader<2>in("ignore_missing_column.csv");
+	in.read_header(io::ignore_missing_column, "a", "b");
+
+  int a,b = 42;
+
+	while(in.read_row(a,b)) {
+	  	std::cout << a << ",";
+	  	std::cout << b << "\n";
+		
+	  // a contains the value from the file
+	  // b is left unchanged by read_row, i.e., it is 42
+	}
+}
