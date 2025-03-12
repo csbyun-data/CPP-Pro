873419d5f285781ab0274536ccb14033c25f1711
csbyun-data <time_series@naver.com>
2025-03-12 16:44:10 +0900

    Update ignore_missing_column.c

--- chap03/CSV/ignore_missing_column.c
+++ chap03/CSV/ignore_missing_column.c
@@ -3,17 +3,19 @@
 
 int main()
 {
-	// The file only contains column "a"
-	io::CSVReader<2>in("ignore_missing_column.csv");
-	in.read_header(io::ignore_missing_column, "a", "b");
-
+  // The file only contains column "a"
+  io::CSVReader<2>in("ignore_missing_column.csv");
+  in.read_header(io::ignore_missing_column, "a", "b");
+  
   int a,b = 42;
+  
+  while(in.read_row(a,b)) {
+    std::cout << a << ",";
+    std::cout << b << "\n";
+    
+    // a contains the value from the file
+    // b is left unchanged by read_row, i.e., it is 42
+  }
 
-	while(in.read_row(a,b)) {
-	  	std::cout << a << ",";
-	  	std::cout << b << "\n";
-		
-	  // a contains the value from the file
-	  // b is left unchanged by read_row, i.e., it is 42
-	}
+  return 0;
 }
