01dfe1ef189ec1241231b7920aa307605dbcebab
csbyun-data <time_series@naver.com>
2025-07-26 14:37:42 +0900

    Update deep_copy_ex02.cpp

--- chap01/Copy/deep_copy_ex02.cpp
+++ chap01/Copy/deep_copy_ex02.cpp
@@ -1,34 +1,36 @@
 class Person {
 private:
-    char* name;
+  char* name;
 
 public:
-    Person(const char* pname) {
-        name = new char[strlen(pname) + 1];
-        strcpy(name, pname);
-    }
-
-    // 복사 생성자 (깊은 복사)
-    Person(const Person& other) {
-        name = new char[strlen(other.name) + 1];
-        strcpy(name, other.name);
-    }
-
-    // 대입 연산자 오버로딩 (깊은 복사)
-    Person& operator=(const Person& other) {
-        if (this != &other) {
-            delete[] name; // 기존 메모리 해제
-            name = new char[strlen(other.name) + 1];
-            strcpy(name, other.name);
-        }
-        return *this;
-    }
-
-    ~Person() {
-        delete[] name;
-    }
-
-    void print() {
-        cout << "Name: " << name << endl;
-    }
+  Person(const char* pname) {
+      name = new char[strlen(pname) + 1];
+      strcpy(name, pname);
+  }
+  
+  // 복사 생성자 (깊은 복사)
+  Person(const Person& other) {
+      name = new char[strlen(other.name) + 1];
+      strcpy(name, other.name);
+  }
+  
+  // 대입 연산자 오버로딩 (깊은 복사)
+  Person& operator=(const Person& other) {
+      if (this != &other) {
+          delete[] name; // 기존 메모리 해제
+          name = new char[strlen(other.name) + 1];
+          strcpy(name, other.name);
+      }
+      return *this;
+  }
+  
+  ~Person() {
+      delete[] name;
+  }
+  
+  void print() {
+      cout << "Name: " << name << endl;
+  }
 };
+
+int main() {
