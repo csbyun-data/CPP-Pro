0250e40b8264f86746fa41bcb52a308fcd330690
csbyun-data <time_series@naver.com>
2025-08-23 08:16:06 +0900

    Update Passing_Array.cpp

--- chap01/Function/Passing_Array.cpp
+++ chap01/Function/Passing_Array.cpp
@@ -1,25 +1,19 @@
+// call by reference , 배열 활용
+
 #include <iostream>
 
-void prt_array(int numbers[], int size);
-void zero_alloc(int numbers[], int size);
+void prt_array(int (&numbers)[5], int size);
 
 int main()
 {
- int my_numbers[] = {1, 2, 3, 4, 5};
- prt_array(my_numbers, 5);
- zero_alloc(my_numbers, 5);
- 
- return 0;
+  int my_numbers[] = {1, 2, 3, 4, 5};
+  prt_array(my_numbers, 5);
+  
+  return 0;
 }
 
-void prt_array(int numbers[], int size) {
- for(int i=0; i < size; i++) {
-  std::cout << numbers[i] << std::endl;
- }
-}
-
-void zero_alloc(int numbers[], int size) {
- for(int i=0; i<size; i++) {
-  numbers[i] = 0;
- }
+void prt_array(int (&numbers)[5], int size) {
+  for(int i=0; i < size; i++) {
+   std::cout << numbers[i] << std::endl;
+  }
 }
