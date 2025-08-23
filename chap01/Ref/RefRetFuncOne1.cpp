2a861e37424c48d013903a3e50e4e4f5bfbaf7b6
csbyun-data <time_series@naver.com>
2025-08-23 21:35:38 +0900

    Create RefRetFuncOne1.cpp

---
+++ chap01/Ref/RefRetFuncOne1.cpp
@@ -0,0 +1,33 @@
+// 윤성우 열혈강의 C++
+// 반환형이 참조이고 반환도 참조로 받는 경우
+
+#include <iostream>
+
+using namespace std;
+
+int & RefReturnFunctionOne( int &ref) {
+	ref++;
+	return ref;
+}
+
+int main(int argc, char** argv) {
+
+	// int num1 = 1;
+	// int &ref = num1;
+	// int num2 = ref;
+		
+	int num1 = 1;
+	int num2 = RefReturnFunctionOne(num1);
+	num1++;
+	num2++;
+	
+	cout << "num1: " << num1 << endl;
+	cout << "num2: " << num2 << endl;
+	
+	return 0;
+}
+
+/* result
+num1: 3
+num2: 3
+*/
